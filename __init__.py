02dbfe7ab266f9ff06e3896f539214445cc5f6d4
Cloud W <98995559+yunkezengren@users.noreply.github.com>
2023-08-23 15:48:06 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,9 @@ bl_info = {
     "author" : "原作者:Kuldeep Singh; 修改:一尘不染 云可赠人",
     "description" : "align node  Shift Q | Ctrl Q",
     "blender" : (2, 83, 0),
-    "version" : (0, 0, 1),
+    "version" : (1, 0, 0),
     "location": "Nodes Editor",
-    "category": "Node",
-    "warning":  "This version is still in development."
+    "category": "Node"
 }
 
 from . import auto_load
