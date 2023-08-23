085071432db41534f77e98e31087f54d51ac41b8
Cloud W <98995559+yunkezengren@users.noreply.github.com>
2023-08-23 13:17:50 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,19 +1,7 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+# https://github.com/yunkezengren/Blender-align-node
 bl_info = {
     "name" : "Node Align",
-    "author" : "Kuldeep Singh",
+    "author" : "原作者:Kuldeep Singh; 修改:一尘不染 云可赠人",
     "description" : "align node  Shift Q | Ctrl Q",
     "blender" : (2, 83, 0),
     "version" : (0, 0, 1),
